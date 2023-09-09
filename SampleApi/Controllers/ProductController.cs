f1a28d9b164715c10ddd89f8a6f1197374c69b6c
Cem Tezcan <cemtzcn35@gmail.com>
2023-09-09 14:56:31 +0300

    feat(configuration of the Api):Add folders and create controller. #ref:1

--- SampleApi/Controllers/ProductController.cs
+++ SampleApi/Controllers/ProductController.cs
@@ -8,12 +8,26 @@ namespace SampleApi.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private static List<ProductEntity> products = new List<ProductEntity>(){
+                new ProductEntity{Id=1, Name="Mobile Phone",Price="10000"},
+                new ProductEntity{Id=2, Name="Desktop Computer",Price="20000"},
+                new ProductEntity{Id=3, Name="Laptop",Price="25000"},
+                new ProductEntity{Id=4, Name="Headphone",Price="350"},
+                new ProductEntity{Id=5, Name="Keyboard",Price="500"}
+            };
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<ActionResult> Get()
         {
-            var products = new List<ProductEntity>{
-                new ProductEntity{Id=1, Name=""}
-            }
+            return Ok(products);
         }
+        [HttpGet("{Id}")]
+        public async Task<ActionResult> Get(int Id)
+        {
+            var product = products.Find(x => x.Id == Id);
+            if (product == null)
+                return BadRequest("No product Id");
+            return Ok(product);
+        }
+
     }
 }
