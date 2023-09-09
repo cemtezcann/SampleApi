b6e62a0ddecf7c3b0e2e233990563898820add25
Cem Tezcan <cemtzcn35@gmail.com>
2023-09-09 10:11:38 +0300

    Add project files.

---
+++ SampleApi/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleApi.Core;
+
+namespace SampleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var products = new List<ProductEntity>{
+                new ProductEntity{Id=1, Name=""}
+            }
+        }
+    }
+}
---
+++ SampleApi/Core/ProductEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace SampleApi.Core
+{
+    public class ProductEntity
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Price { get; set; }
+
+    }
+}
