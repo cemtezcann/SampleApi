6368e73a6e2d8e1e8caa7f985d594b8b47992c81
Cem Tezcan <cemtzcn35@gmail.com>
2023-09-11 02:41:15 +0300

    feat(Migration) : Add migrations of dbcontext

--- SampleApi/Data/DataContext.cs
+++ SampleApi/Data/DataContext.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using SampleApi.Core;
+﻿using SampleApi.Core;
 
 namespace SampleApi.Data
 {
--- SampleApi/Program.cs
+++ SampleApi/Program.cs
@@ -1,4 +1,9 @@
+global using SampleApi.Data;
+global using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
