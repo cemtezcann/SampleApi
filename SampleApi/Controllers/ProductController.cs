3accfe6172e7b215bb93431d0c641e00625b4f00
Cem Tezcan <cemtzcn35@gmail.com>
2023-09-11 23:31:18 +0300

    feat(productController): Add and confıgure to productController. #ref 3

--- SampleApi/Controllers/ProductController.cs
+++ SampleApi/Controllers/ProductController.cs
@@ -8,22 +8,23 @@ namespace SampleApi.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private static List<ProductEntity> products = new List<ProductEntity>(){
-                new ProductEntity{Id=1, Name="Mobile Phone",Price="10000"},
-                new ProductEntity{Id=2, Name="Desktop Computer",Price="20000"},
-                new ProductEntity{Id=3, Name="Laptop",Price="25000"},
-                new ProductEntity{Id=4, Name="Headphone",Price="350"},
-                new ProductEntity{Id=5, Name="Keyboard",Price="500"}
-            };
+        private readonly DataContext _context;
+        public ProductController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+
         [HttpGet]
         public async Task<ActionResult<List<ProductEntity>>> Get()  // Bring the all product in the Product List
         {
-            return Ok(products);
+            return Ok(await _context.productEntities.ToListAsync());
         }
         [HttpGet("{Id}")]
         public async Task<ActionResult<ProductEntity>> Get(int Id)  // Bring the product of Determined Id
         {
-            var product = products.Find(x => x.Id == Id); // It searches one by one until it found finally bring the product or return bad request.
+            var product = await _context.productEntities.FindAsync(Id); // It searches one by one until it found finally bring the product or return bad request.
             if (product == null)
                 return BadRequest("No product Id");
             return Ok(product);
@@ -32,28 +33,31 @@ namespace SampleApi.Controllers
         [HttpPost]
         public async Task<ActionResult<List<ProductEntity>>> AddProduct(ProductEntity product)  // Let us add the new product on the List
         {
-            products.Add(product);
-            return Ok(products);
+            _context.productEntities.Add(product);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.productEntities.ToListAsync());
         }
 
         [HttpPut]
         public async Task<ActionResult<List<ProductEntity>>> UpdateProduct(ProductEntity request)
         {
-            var product = products.Find(x => x.Id == request.Id);
+            var product = await _context.productEntities.FindAsync(request.Id);
             if (product == null)
                 return BadRequest("No Id it will be changed");
             product.Name = request.Name; ;
             product.Price = request.Price;
-            return Ok(products);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.productEntities.ToListAsync());
         }
-        [HttpDelete]
+        [HttpDelete("{Id}")]
         public async Task<ActionResult<List<ProductEntity>>> DeleteProduct(int Id)
         {
-            var product = products.Find( x => x.Id == Id);
+            var product = await _context.productEntities.FindAsync(Id);
             if (product == null)
                 return BadRequest("No Id it will be deleted");
-            products.Remove(product);
-            return Ok(products);
+            _context.productEntities.Remove(product);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.productEntities.ToListAsync());
         }
         
 
